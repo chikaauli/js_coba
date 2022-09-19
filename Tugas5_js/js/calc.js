3d07ed9bdbdc313946480e2f4fbd2316792f0a48
chika aulia zahwa <100188919+chikaauli@users.noreply.github.com>
2022-09-19 21:13:35 +0700

    Add files via upload

---
+++ Tugas5_js/js/calc.js
@@ -0,0 +1,48 @@
+function hitung(){
+    //tangkap id form
+    var frm = document.getElementById('calcForm');
+    var a1 = parseFloat(frm.angka1.value);
+    var a2 = parseFloat(frm.angka2.value);
+
+    var tombol = frm.tombol.value;
+    switch (tombol) {
+        case 'btn_tambah':
+            if (isNaN(a1) || isNaN(a2)) {
+                alert("Harap masukkan angka!!");
+            }    
+            else {
+                var total = a1 + a2; //rumus
+                frm.hasil.value = total; //penempatan hasil 
+            } 
+            break;
+        case 'btn_kurang':
+            if (isNaN(a1) || isNaN(a2)) {
+                alert("Harap masukkan angka!!");
+            }    
+            else {
+                var total = a1 - a2; //rumus
+                frm.hasil.value = total; //penempatan hasil 
+            } 
+            break;
+    
+        default:
+            break;
+    }
+    // if (isNaN(a1) || isNaN(a2)) {
+    //     alert("Harap masukkan angka!!");
+    // }    
+    // else {
+    //    var total = a1 - a2; //rumus
+    //    frm.hasil.value = total; //penempatan hasil 
+    // } 
+    
+}
+
+// function pangkat(){
+//     //tangkap id form
+//     var frm = document.getElementById('calcForm');
+//     var a1 = parseFloat(frm.angka1.value);
+//     var a2 = parseFloat(frm.angka2.value);
+//     var total = Math.pow(a1,a2); //rumus
+//     frm.hasil.value = total; //penempatan hasil
+// }
